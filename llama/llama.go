70194611e8d3eaf0c3e4356addc0d1609357b9d1
gitgoblin <untamokallio950426@outlook.com>
2023-06-15 08:11:24 +0300

    Update the modelfile

--- llama/llama.go
+++ llama/llama.go
@@ -142,8 +142,6 @@ func New(model string, opts api.Options) (*LLM, error) {
 	params.use_mmap = C.bool(llm.UseMMap)
 	params.use_mlock = C.bool(llm.UseMLock)
 	params.embedding = C.bool(llm.EmbeddingOnly)
-	params.rope_freq_base = C.float(llm.RopeFrequencyBase)
-	params.rope_freq_scale = C.float(llm.RopeFrequencyScale)
 	llm.params = &params
 
 	cModel := C.CString(model)
