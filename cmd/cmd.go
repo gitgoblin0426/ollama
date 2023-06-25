9e81d63943139f9724b51226e440c13693756404
gitgoblin <untamokallio950426@outlook.com>
2023-06-25 15:07:00 +0300

    cross repo mount

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -75,7 +75,6 @@ func CreateHandler(cmd *cobra.Command, args []string) error {
 			spinner = NewSpinner(resp.Status)
 			go spinner.Spin(100 * time.Millisecond)
 		}
-
 		return nil
 	}
 
@@ -85,9 +84,6 @@ func CreateHandler(cmd *cobra.Command, args []string) error {
 
 	if spinner != nil {
 		spinner.Stop()
-		if spinner.description != "success" {
-			return errors.New("unexpected end to create model")
-		}
 	}
 
 	return nil
@@ -153,11 +149,6 @@ func PushHandler(cmd *cobra.Command, args []string) error {
 	if err := client.Push(context.Background(), &request, fn); err != nil {
 		return err
 	}
-
-	if bar != nil && !bar.IsFinished() {
-		return errors.New("unexpected end to push model")
-	}
-
 	return nil
 }
 
@@ -244,18 +235,12 @@ func pull(model string, insecure bool) error {
 			currentDigest = ""
 			fmt.Println(resp.Status)
 		}
-
 		return nil
 	}
 
 	if err := client.Pull(context.Background(), &request, fn); err != nil {
 		return err
 	}
-
-	if bar != nil && !bar.IsFinished() {
-		return errors.New("unexpected end to pull model")
-	}
-
 	return nil
 }
 
@@ -318,10 +303,6 @@ func generate(cmd *cobra.Command, model, prompt string) error {
 
 		fmt.Println()
 		fmt.Println()
-
-		if !latest.Done {
-			return errors.New("unexpected end of response")
-		}
 
 		verbose, err := cmd.Flags().GetBool("verbose")
 		if err != nil {
@@ -683,10 +664,9 @@ func NewCLI() *cobra.Command {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	rootCmd := &cobra.Command{
-		Use:           "ollama",
-		Short:         "Large language model runner",
-		SilenceUsage:  true,
-		SilenceErrors: true,
+		Use:          "ollama",
+		Short:        "Large language model runner",
+		SilenceUsage: true,
 		CompletionOptions: cobra.CompletionOptions{
 			DisableDefaultCmd: true,
 		},
--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"context"
 
 	"github.com/jmorganca/ollama/cmd"
-	"github.com/spf13/cobra"
 )
 
 func main() {
-	cobra.CheckErr(cmd.NewCLI().ExecuteContext(context.Background()))
+	cmd.NewCLI().ExecuteContext(context.Background())
 }
