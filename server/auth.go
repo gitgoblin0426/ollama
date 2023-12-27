3ebe6ff816ace0c9fdce654507f1b0d48180fecc
gitgoblin <untamokallio950426@outlook.com>
2023-12-27 13:46:56 +0200

    Harden the OLLAMA_HOST lookup for quotes

--- server/auth.go
+++ server/auth.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"crypto/rand"
 	"crypto/sha256"
@@ -9,87 +10,167 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
+	"golang.org/x/crypto/ssh"
+
 	"github.com/jmorganca/ollama/api"
-	"github.com/jmorganca/ollama/auth"
 )
 
-type registryChallenge struct {
+type AuthRedirect struct {
 	Realm   string
 	Service string
 	Scope   string
 }
 
-func (r registryChallenge) URL() (*url.URL, error) {
+type SignatureData struct {
+	Method string
+	Path   string
+	Data   []byte
+}
+
+func generateNonce(length int) (string, error) {
+	nonce := make([]byte, length)
+	_, err := rand.Read(nonce)
+	if err != nil {
+		return "", err
+	}
+	return base64.RawURLEncoding.EncodeToString(nonce), nil
+}
+
+func (r AuthRedirect) URL() (*url.URL, error) {
 	redirectURL, err := url.Parse(r.Realm)
 	if err != nil {
 		return nil, err
 	}
 
 	values := redirectURL.Query()
+
 	values.Add("service", r.Service)
+
 	for _, s := range strings.Split(r.Scope, " ") {
 		values.Add("scope", s)
 	}
 
 	values.Add("ts", strconv.FormatInt(time.Now().Unix(), 10))
 
-	nonce, err := auth.NewNonce(rand.Reader, 16)
+	nonce, err := generateNonce(16)
 	if err != nil {
 		return nil, err
 	}
-
 	values.Add("nonce", nonce)
 
 	redirectURL.RawQuery = values.Encode()
 	return redirectURL, nil
 }
 
-func getAuthorizationToken(ctx context.Context, challenge registryChallenge) (string, error) {
-	redirectURL, err := challenge.URL()
+func getAuthToken(ctx context.Context, redirData AuthRedirect) (string, error) {
+	redirectURL, err := redirData.URL()
 	if err != nil {
 		return "", err
 	}
 
-	sha256sum := sha256.Sum256(nil)
-	data := []byte(fmt.Sprintf("%s,%s,%s", http.MethodGet, redirectURL.String(), base64.StdEncoding.EncodeToString([]byte(hex.EncodeToString(sha256sum[:])))))
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	keyPath := filepath.Join(home, ".ollama", "id_ed25519")
+
+	rawKey, err := os.ReadFile(keyPath)
+	if err != nil {
+		slog.Info(fmt.Sprintf("Failed to load private key: %v", err))
+		return "", err
+	}
+
+	s := SignatureData{
+		Method: http.MethodGet,
+		Path:   redirectURL.String(),
+		Data:   nil,
+	}
+
+	sig, err := s.Sign(rawKey)
+	if err != nil {
+		return "", err
+	}
 
 	headers := make(http.Header)
-	signature, err := auth.Sign(ctx, data)
+	headers.Set("Authorization", sig)
+	resp, err := makeRequest(ctx, http.MethodGet, redirectURL, headers, nil, nil)
 	if err != nil {
+		slog.Info(fmt.Sprintf("couldn't get token: %q", err))
 		return "", err
 	}
+	defer resp.Body.Close()
 
-	headers.Add("Authorization", signature)
-
-	response, err := makeRequest(ctx, http.MethodGet, redirectURL, headers, nil, nil)
-	if err != nil {
-		return "", err
-	}
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return "", fmt.Errorf("%d: %v", response.StatusCode, err)
-	}
-
-	if response.StatusCode >= http.StatusBadRequest {
-		if len(body) > 0 {
-			return "", fmt.Errorf("%d: %s", response.StatusCode, body)
-		} else {
-			return "", fmt.Errorf("%d", response.StatusCode)
+	if resp.StatusCode >= http.StatusBadRequest {
+		responseBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", fmt.Errorf("%d: %v", resp.StatusCode, err)
+		} else if len(responseBody) > 0 {
+			return "", fmt.Errorf("%d: %s", resp.StatusCode, responseBody)
 		}
+
+		return "", fmt.Errorf("%s", resp.Status)
 	}
 
-	var token api.TokenResponse
-	if err := json.Unmarshal(body, &token); err != nil {
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
 		return "", err
 	}
 
-	return token.Token, nil
+	var tok api.TokenResponse
+	if err := json.Unmarshal(respBody, &tok); err != nil {
+		return "", err
+	}
+
+	return tok.Token, nil
+}
+
+// Bytes returns a byte slice of the data to sign for the request
+func (s SignatureData) Bytes() []byte {
+	// We first derive the content hash of the request body using:
+	//     base64(hex(sha256(request body)))
+
+	hash := sha256.Sum256(s.Data)
+	hashHex := make([]byte, hex.EncodedLen(len(hash)))
+	hex.Encode(hashHex, hash[:])
+	contentHash := base64.StdEncoding.EncodeToString(hashHex)
+
+	// We then put the entire request together in a serialize string using:
+	//       "<method>,<uri>,<content hash>"
+	// e.g.  "GET,http://localhost,OTdkZjM1O..."
+
+	return []byte(strings.Join([]string{s.Method, s.Path, contentHash}, ","))
+}
+
+// SignData takes a SignatureData object and signs it with a raw private key
+func (s SignatureData) Sign(rawKey []byte) (string, error) {
+	signer, err := ssh.ParsePrivateKey(rawKey)
+	if err != nil {
+		return "", err
+	}
+
+	// get the pubkey, but remove the type
+	pubKey := ssh.MarshalAuthorizedKey(signer.PublicKey())
+	parts := bytes.Split(pubKey, []byte(" "))
+	if len(parts) < 2 {
+		return "", fmt.Errorf("malformed public key")
+	}
+
+	signedData, err := signer.Sign(nil, s.Bytes())
+	if err != nil {
+		return "", err
+	}
+
+	// signature is <pubkey>:<signature>
+	sig := fmt.Sprintf("%s:%s", bytes.TrimSpace(parts[1]), base64.StdEncoding.EncodeToString(signedData.Blob))
+	return sig, nil
 }
